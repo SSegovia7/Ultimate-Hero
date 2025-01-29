ecba3acb61b5f0657894e2fc04dd9873ad5c4ae1
Aaron Phung <user.name>
2025-01-28 19:57:14 -0800

    Implemented CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Transform targetTransform;
+    [Header("Camera Attributes")]
+    [Tooltip("Maximum distance that the target can be from the center before the camera starts moving")]
+    [SerializeField] private float maxDistanceFromCenter = 1.0f;
+    [SerializeField] private float leftLimit = -5.0f;
+    [SerializeField] private float rightLimit = 5.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,13 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = transform.position.x - targetTransform.position.x;
+        if (distance > maxDistanceFromCenter)
+        {
+            transform.position = new Vector3(Mathf.Max(targetTransform.position.x + maxDistanceFromCenter, leftLimit), transform.position.y, transform.position.z);
+        } else if (distance < -maxDistanceFromCenter)
+        {
+            transform.position = new Vector3(Mathf.Min(targetTransform.position.x - maxDistanceFromCenter, rightLimit), transform.position.y, transform.position.z);
+        }
     }
 }
