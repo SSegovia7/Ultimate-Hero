466e827d777cfbc7e3c6807b81d62ac6506745f5
GameMatador <marvinmata3634@gmail.com>
2025-02-06 18:52:09 -0800

    Simple HealthBar Added
    Red HealthBar UI added to game. Pressing Space lowers the  player's health by one unit. The lowered health is reflected in the HealthBar

---
+++ Assets/Health.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] int currentHealth = 100;
+    [SerializeField] int maxHealth = 100;
+    
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
