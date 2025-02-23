3ab5fbec49c561cd0a6d7d7774e52cbd52755c6d
GameMatador <marvinmata3634@gmail.com>
2025-02-23 12:33:35 -0800

    Hit Detection Implemented

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -25,7 +25,7 @@ public class Health : MonoBehaviour
         }
     }
 
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
