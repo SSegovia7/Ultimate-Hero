7460e6bf5bef5203c9db359805677f82bd54d50b
GameMatador <marvinmata3634@gmail.com>
2025-02-23 16:13:19 -0800

    Player and Enemy Health 0HP Response
    Player and Enemies disappear when at 0 hp or less
    Returns after 2 secs.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -10,6 +10,8 @@ public class Health : MonoBehaviour
     
     public HealthBar healthBar;
 
+    public bool isLiving = true;
+
     void Start()
     {
         currentHealth = maxHealth;
@@ -23,11 +25,38 @@ public class Health : MonoBehaviour
         {
             TakeDamage(20);
         }
+
+        if (currentHealth <= 0 && isLiving == true){
+            Die();
+            isLiving = false;
+        }
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+    }
+    
+    public void Die()
+    {
+        Revive();
+        Invoke("Revive", 2.0f);
+    }
+
+    public void Revive()
+    {
+        if (transform.tag == "Player"){
+            Debug.Log($"{transform.name}: has died");
+            Transform player_sprite_holder = this.transform.Find("Main Character");
+            SpriteRenderer player_renderer = player_sprite_holder.GetComponent<SpriteRenderer>(); //gets player sprite renderer
+            player_renderer.enabled = !player_renderer.enabled; //flips player sprite renderer
+        }
+        else if (transform.tag == "Enemy"){
+            Debug.Log($"{transform.name}: has died");
+            this.GetComponent<SpriteRenderer>().enabled = !this.GetComponent<SpriteRenderer>().enabled;
+        }
+
+
     }
 }
