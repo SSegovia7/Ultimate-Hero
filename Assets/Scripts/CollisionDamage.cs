8879a1537fc9e5e7f5793e7fb66d9ad598f99be2
GameMatador <marvinmata3634@gmail.com>
2025-02-23 16:57:38 -0800

    Collision Damage Inspector Value Added
    Collision Damage Script found in the enemy game objects

--- Assets/Scripts/CollisionDamage.cs
+++ Assets/Scripts/CollisionDamage.cs
@@ -6,10 +6,11 @@ public class CollisionDamage : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] Health player_health;
+    [SerializeField] int collision_damage = 20;
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Player"){
-            player_health.TakeDamage(20);
+            player_health.TakeDamage(collision_damage);
         }else if (other.gameObject.tag == "Enemy"){
             
         }
