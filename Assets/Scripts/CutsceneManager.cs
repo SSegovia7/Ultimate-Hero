dedca417b997ecb0279b314c2d72f564dfb0f391
Aaron Phung <user.name>
2025-02-27 15:03:48 -0800

    implemented simple cutscene functionality; displays different images with a set delay, then loads into gameplay scene

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CutsceneManager : MonoBehaviour
 {
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Sprite[] images;
+    [SerializeField] private float delayBetweenImages = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("RunCutscene");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator RunCutscene()
+    {
+        foreach (Sprite image in images)
+        {
+            backgroundImage.sprite = image;
+            yield return new WaitForSeconds(delayBetweenImages);
+        }
+        SceneManager.LoadScene(1);
+        yield return null;
     }
 }
