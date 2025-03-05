30f7fb9909517787df8988a19def88612aecf743
Aaron Phung <user.name>
2025-03-04 22:11:02 -0800

    implement functionality for pathfinding to and following the player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private NodeController nodeController;
+    [SerializeField] private float pathRefreshTimeout = 1f;
     private Node closestNode = null;
+    public List<Node> path = new List<Node>();
+
+    public float pathTime = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pathTime = 0f;
     }
 
     // Update is called once per frame
@@ -21,6 +25,41 @@ public class EnemyAI : MonoBehaviour
 
     private void FixedUpdate()
     {
+        pathTime += Time.deltaTime;
+        CreatePath();
+    }
+
+    public void CreatePath()
+    {
         closestNode = nodeController.closestNode;
+        if (path.Count > 0 && pathTime < pathRefreshTimeout)
+        {
+            int x = 0;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(path[x].transform.position.x, path[x].transform.position.y, 0), 0.1f);
+
+            if (Vector2.Distance(transform.position, path[x].transform.position) < 0.1f)
+            {
+                closestNode = path[x];
+                path.RemoveAt(x);
+            }
+        }
+        else if (pathTime >= pathRefreshTimeout)
+        {
+            Node target = EnemyManager.instance.playerNodeController.closestNode;
+            if (target != null)
+            {
+                path = AStarManager.instance.GeneratePath(closestNode, target);
+            }
+            pathTime = 0;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        for (int i = 0; i < path.Count - 1; ++i)
+        {
+            Gizmos.DrawLine(path[i].transform.position, path[i + 1].transform.position);
+        }
     }
 }
