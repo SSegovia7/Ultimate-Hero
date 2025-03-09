760e46a37cbd0c09dffdb2c674515ff3732edf03
aaronphung998 <wacky700@gmail.com>
2025-03-08 17:19:54 -0800

    implemented SpawnEnemy

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private EnemyManager enemyManager;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,15 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // returns the EnemyAI component of the spawned enemy, for use with the EnemyManager that calls this function
+    public EnemyAI SpawnEnemy(GameObject enemyPrefab)
+    {
+        GameObject spawnedEnemy = Instantiate(enemyPrefab, transform);
+        EnemyAI enemyAI = spawnedEnemy.GetComponent<EnemyAI>();
+        enemyAI.enemyManager = enemyManager;
+        return enemyAI;
+
     }
 }
