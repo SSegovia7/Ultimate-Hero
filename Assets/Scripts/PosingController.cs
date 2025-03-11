67468ef8189e1884a4aa235954483f6e0cca85e4
Samuel Segovia <fightingfefnir191@gmail.com>
2025-03-11 02:17:06 -0700

    fixed build stuff

--- Assets/Scripts/PosingController.cs
+++ Assets/Scripts/PosingController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
-using TMPro.EditorUtilities;
+//using TMPro.EditorUtilities;
 using Unity.VisualScripting;
 using UnityEngine;
 using System.Threading;
